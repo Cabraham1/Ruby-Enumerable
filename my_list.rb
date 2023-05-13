69bb96e92ee50bb0182acb3904f76c06051b082b
Kola Kolade <kolakolade@gmail.com>
2023-05-13 22:00:18 +0100

    Add construct for class Mylist

---
+++ my_list.rb
@@ -0,0 +1,23 @@
+require_relative 'module'
+class Mylist
+  include MyEnumerable
+  def initialize(*list)
+    @list = list
+  end
+end
+# This is for all method
+list = Mylist.new(1, 2, 3, 4)
+list.all? { |e| e < 5 } # => true
+list.all? { |e| e > 5 } # => false
+# This is for any method
+list.any? { |e| e == 2 } # => true
+list.any? { |e| e == 5 } # => false
+# This is for filter method
+list.filter?(&:even?) # [2, 4]
+
+
+
+
+
+
+
